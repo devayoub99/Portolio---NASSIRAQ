c089ab2a0b67fdb11e9c110b80f9d82ca8c842e3
Ayoub <admin@dev-ayoub.com>
2024-01-24 20:42:34 +0300

    Remove Empty link (home)

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,20 +1,31 @@
 import { links } from "../data";
 
 export default function Links({ className }) {
-  const isMobile = className === "mobile-links" ? true : false;
+  const isMobile = className === "mobile-links";
 
   return (
     <ul className={className}>
       {links.map((link) => {
-        return (
-          <li className={isMobile ? "icon-link" : "link"} key={link.id}>
+        return isMobile ? (
+          <li className="icon-link" key={link.id}>
             <a
               href={link.linkTo}
               target={link.title === "Resume" ? "_blank" : undefined}
             >
-              {isMobile ? <img src={link.icon} alt={link.title} /> : link.title}
+              <img src={link.icon} alt={link.title} />
             </a>
           </li>
+        ) : (
+          link.title && (
+            <li className="link" key={link.id}>
+              <a
+                href={link.linkTo}
+                target={link.title === "Resume" ? "_blank" : undefined}
+              >
+                {link.title}
+              </a>
+            </li>
+          )
         );
       })}
     </ul>
