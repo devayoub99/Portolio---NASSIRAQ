5b71291bd44859550d19616af780c7193931c4d2
Ayoub <admin@dev-ayoub.com>
2024-01-27 22:44:36 +0300

    Use the imported animation variants (animation.js)

--- src/components/layout/Skills.jsx
+++ src/components/layout/Skills.jsx
@@ -1,21 +1,10 @@
 import { motion as m } from "framer-motion";
+
 import developerImage from "../../assets/developer.webp";
 import { skills } from "../../data";
-import Skill from "../Skill";
+import { fadeInVariants } from "../../animations";
 
-const fadeInVariants = {
-  from: {
-    opacity: 0,
-    y: 100,
-  },
-  to: (index) => ({
-    opacity: 1,
-    y: 0,
-    transition: {
-      delay: 0.2 * index,
-    },
-  }),
-};
+import Skill from "../Skill";
 
 const fadeInSkillVariants = {
   from: {
